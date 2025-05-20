b95c1221454bfa0cbe23b8f79880c345db610913
YacineLaribi <lyacine230@gmail.com>
2025-05-20 02:17:11 +0100

    adding some changes

--- config/settings.py
+++ config/settings.py
@@ -118,6 +118,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Media files (Uploaded files)
+# https://docs.djangoproject.com/en/5.2/ref/settings/#media-root
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- core/views.py
+++ core/views.py
@@ -34,11 +34,32 @@ def logout_view(request):
 from .models import Challenge
 def challenges(request):
     challenges = Challenge.objects.all()  # Assuming you have a Challenge model
+    for challenge in challenges:
+        challenge.is_submitted = Submission.objects.filter(user=request.user, challenge=challenge).exists()
+
     return render(request, 'core/challenges.html', {'challenges': challenges})
 
+from .models import Hint, Submission
 def challenge_details(request , pk):
     challenge = Challenge.objects.get(pk=pk)  # Assuming you have a Challenge model
-    
+    is_submitted = False
     hints = challenge.hint_set.all()  
 
-    return render(request, 'core/challenge_details.html', {'challenge': challenge, 'hints': hints})
+    if request.method == 'POST':
+        diagnosis = request.POST.get('diagnosis')
+        user = request.user
+        if user.is_authenticated:
+
+            submission = Submission(user=user, challenge=challenge, solution=diagnosis)
+            submission.save()
+            messages.success(request, 'Your submission has been recorded.')
+        else:
+            messages.error(request, 'You need to be logged in to submit a solution.')
+
+    if request.user.is_authenticated:
+        is_submitted = Submission.objects.filter(user=request.user, challenge=challenge).exists()
+    else:
+        is_submitted = False
+
+
+    return render(request, 'core/challenge_details.html', {'challenge': challenge, 'hints': hints , 'is_submitted': is_submitted})
